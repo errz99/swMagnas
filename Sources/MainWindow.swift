afbe39b1bdf66d2a5c74a6cc9c2336b37698ed1a
errz99 <errz99@gmail.com>
2025-05-19 16:54:17 +0200

    create radio buttons and make other changes in main window

--- Sources/MainWindow.swift
+++ Sources/MainWindow.swift
@@ -11,16 +11,33 @@ class DrawingView: NSView {
   }
 }
 
-class MainWindow: NSWindow {
+// Subclase de NSButton para deshabilitar el foco
+class NonFocusableRadioButton: NSButton {
+  override var acceptsFirstResponder: Bool {
+    return false  // Evita que el radio button acepte el foco
+  }
+}
+
+class MainWindow: NSWindow, NSWindowDelegate {
+  var radioButtonAll: NonFocusableRadioButton!
+  var radioButtonDirs: NonFocusableRadioButton!
+  var radioButtonFiles: NonFocusableRadioButton!
+  var drawingView: DrawingView!
+
   init(frame: NSRect) {
     super.init(
       contentRect: frame,
       styleMask: [.titled, .closable, .resizable, .miniaturizable],
       backing: .buffered,
       defer: false
+
     )
 
+    // Configura el delegado de la ventana
+    self.delegate = self
+
     let sep: CGFloat = 8
+    let textColor = NSColor.init(red: 0.3, green: 0.7, blue: 0.3, alpha: 1.0)
 
     // Configurar título y propiedades de la ventana
     self.title = "untitled - swMagnas"
@@ -32,12 +49,47 @@ class MainWindow: NSWindow {
     topStackView.spacing = sep
     topStackView.distribution = .fillEqually
 
-    // Agregar elementos al StackView
-    let topLeftTextField = NSTextField(labelWithString: "All - Dirs - Files")
-    topLeftTextField.alignment = .left
+    // Crear un contenedor para los botones de radio
+    let radioButtonContainer = NSStackView()
+    radioButtonContainer.orientation = .horizontal
+    radioButtonContainer.alignment = .centerY
+    radioButtonContainer.spacing = sep
+
+    // Crear los botones de radio
+    radioButtonAll = NonFocusableRadioButton(radioButtonWithTitle: "All", target: nil, action: nil)
+    radioButtonDirs = NonFocusableRadioButton(
+      radioButtonWithTitle: "Dirs", target: nil, action: nil)
+    radioButtonFiles = NonFocusableRadioButton(
+      radioButtonWithTitle: "Files", target: nil, action: nil)
+
+    // Agregar los botones de radio al contenedor
+    radioButtonContainer.addView(radioButtonAll, in: .leading)
+    radioButtonContainer.addView(radioButtonDirs, in: .leading)
+    radioButtonContainer.addView(radioButtonFiles, in: .leading)
+
+    // Configurar los radio buttons predeterminados
+    radioButtonAll.state = .on  // 'All' será seleccionado por defecto
+    radioButtonDirs.state = .off
+    radioButtonFiles.state = .off
+
+    // Configurar acciones para los radio buttons
+    radioButtonAll.target = self
+    radioButtonAll.action = #selector(setStateForRadioButtons(_:))
+    radioButtonDirs.target = self
+    radioButtonDirs.action = #selector(setStateForRadioButtons(_:))
+    radioButtonFiles.target = self
+    radioButtonFiles.action = #selector(setStateForRadioButtons(_:))
+
+    // Buscador
     let searchField = NSSearchField()
     searchField.alignment = .center
+    searchField.target = self
+    searchField.action = #selector(handleSearchFieldAction(_:))
+
+    // Etiquetas de volumen
     let topRightTextField = NSTextField(labelWithString: "Derecha")
+    topRightTextField.font = NSFont.systemFont(ofSize: 12)
+    topRightTextField.textColor = textColor
     topRightTextField.alignment = .right
 
     // Configurar dimensiones por defecto del NSSearchField
@@ -49,7 +101,8 @@ class MainWindow: NSWindow {
         equalToConstant: searchField.intrinsicContentSize.height),
     ])
 
-    topStackView.addView(topLeftTextField, in: .leading)
+    // topStackView.addView(topLeftTextField, in: .leading)
+    topStackView.addView(radioButtonContainer, in: .leading)
     topStackView.addView(searchField, in: .center)
     topStackView.addView(topRightTextField, in: .trailing)
 
@@ -62,10 +115,14 @@ class MainWindow: NSWindow {
 
     // Agregar elementos al StackView
     let bottomLeftTextField = NSTextField(labelWithString: "v0.0.1-20250518")
+    bottomLeftTextField.font = NSFont.systemFont(ofSize: 11)
     bottomLeftTextField.alignment = .left
     let bottomCenterTextField = NSTextField(labelWithString: "0/0")
+    bottomCenterTextField.font = NSFont.systemFont(ofSize: 11)
     bottomCenterTextField.alignment = .center
     let bottomRightTextField = NSTextField(labelWithString: "on:")
+    bottomRightTextField.font = NSFont.systemFont(ofSize: 11)
+    bottomRightTextField.textColor = textColor
     bottomRightTextField.alignment = .right
 
     bottomStackView.addView(bottomLeftTextField, in: .leading)
@@ -73,7 +130,7 @@ class MainWindow: NSWindow {
     bottomStackView.addView(bottomRightTextField, in: .trailing)
 
     // Crear vista de dibujo
-    let drawingView = DrawingView()
+    drawingView = DrawingView()
 
     // Crear el StackView vertical
     let verticalStackView = NSStackView()
@@ -87,6 +144,8 @@ class MainWindow: NSWindow {
 
     // Añadir el StackView al contentView
     self.contentView?.addSubview(verticalStackView)
+    // Configurar el foco inicial en searchField
+    self.contentView?.window?.makeFirstResponder(searchField)
 
     // Configurar restricciones del StackView (top)
     topStackView.translatesAutoresizingMaskIntoConstraints = false
@@ -108,5 +167,54 @@ class MainWindow: NSWindow {
       bottomStackView.bottomAnchor.constraint(
         equalTo: self.contentView!.bottomAnchor, constant: -sep),
     ])
+  }
+
+  // Sobrescribir el método keyDown para capturar eventos de teclado
+  override func keyDown(with event: NSEvent) {
+    switch event.keyCode {
+    case 122:  // F1
+      print("F1")
+      activateRadioButton(radioButtonAll)
+    case 120:  // F2
+      activateRadioButton(radioButtonDirs)
+    case 99:  // F3
+      activateRadioButton(radioButtonFiles)
+    default:
+      print("key code: \(event.keyCode)")
+      super.keyDown(with: event)  // Pasar otros eventos al sistema
+    }
+  }
+
+  // Activar el radio button especificado
+  private func activateRadioButton(_ button: NonFocusableRadioButton) {
+    setStateForRadioButtons(button)
+    button.performClick(nil)  // Simular un clic en el botón
+  }
+
+  // Método para manejar el cambio de estado de los radio buttons
+  @objc func setStateForRadioButtons(_ sender: NonFocusableRadioButton) {
+    // Desactivar todos los botones primero
+    self.radioButtonAll.state = .off
+    self.radioButtonDirs.state = .off
+    self.radioButtonFiles.state = .off
+
+    // Activar únicamente el botón seleccionado
+    sender.state = .on
+  }
+
+  // Método del delegado que responde al evento de redimensionar
+  func windowDidResize(_ notification: Notification) {
+    let width = Int(self.frame.size.width)
+    let height = Int(self.frame.size.height)
+
+    if height % 50 == 0 {
+      print("Dimensiones de la ventana - Width: \(width), Height: \(height)")
+    }
+  }
+
+  // Define el método para manejar el evento de pulsar la tecla return
+  @objc func handleSearchFieldAction(_ sender: NSSearchField) {
+    print("Texto del searchField: \(sender.stringValue)")
+    self.makeFirstResponder(self.drawingView)
   }
 }
